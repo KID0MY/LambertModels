29fe304ed8ffc6dbeee6a7a86bd3190ea6180a57
Lucas Triboli Bujes <57596595+KID0MY@users.noreply.github.com>
2025-10-02 21:48:24 -0400

    project

---
+++ Assets/ChangeBetweenMateiral.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ChangeBetweenMateiral : MonoBehaviour
+{
+    public Material[] materials;
+
+    [SerializeField] public int shader;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
